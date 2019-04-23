255941f263a9e90a96f5e6ea5f8c395f3256ee14
owl0504 <854124070@qq.com>
2019-04-23 12:02:53 +0800

    springboot test

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("test hello");
+        System.out.println("hello test");
     }
 }
